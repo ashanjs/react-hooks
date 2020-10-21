6482b8b190b9c9f6edea0b40f43cb024d96cc966
Asha <94jsasha@gmail.com>
2020-10-21 14:35:22 +0200

    Add exercise 01 extra-1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,9 +3,9 @@
 
 import React from 'react'
 
-function Greeting() {
+function Greeting({ initialName = '' }) {
 
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
     setName(event.target.value);
@@ -15,7 +15,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +23,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Asha" />
 }
 
 export default App
