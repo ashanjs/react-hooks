0dabde1e1111578a65ec3b63f4a343a4c4d71ed0
Asha <94jsasha@gmail.com>
2020-10-27 20:56:19 +0100

    Add exercise 06 extra-2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -12,37 +12,41 @@ function PokemonInfo({ pokemonName }) {
   // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
+
 
   React.useEffect(() => {
     if (!pokemonName) {
       return
     }
-    setPokemon(null)
-    setError(null)
-
+    setStatus('pending')
     fetchPokemon(pokemonName).then(
-      pokemon => setPokemon(pokemon),
-      error => setError(error)
+      pokemon => {
+        setStatus('resolved')
+        setPokemon(pokemon)
+      },
+      error => {
+        setError(error)
+        setStatus('rejected')
+      }
     )
-
   }, [pokemonName])
 
-  if (error) {
+  if (status === 'idle') {
+    return "Submit a pokemon"
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  }
+  else if (status === 'rejected') {
     return (
       <div role="alert">
         There was an error: <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
       </div>)
   }
-  else if (!pokemonName) {
-    return "Sumbit a pokemon"
-  } else if (!pokemon) {
-    return <PokemonInfoFallback name={pokemonName} />
-  }
-  else {
+  else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
   }
-
-
+  throw new Error('This should be impossible')
 }
 
 function App() {
